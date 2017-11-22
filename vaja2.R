0d8089d6ecab49b869870a5c20873094d113fe64
LampicJ15 <jan.lampic13@gmail.com>
2017-11-22 23:10:45 +0100

    zakljucek 2.vaje

--- vaja2.R
+++ vaja2.R
@@ -3,26 +3,27 @@ library(readr)
 library(actuar)
 library(readtext)
 
-##1.naloga
+##=========================================================
+#1.naloga
 
 #a.)
 #uvozimo podatke
-vzorec <- read.table("Vaja2/vzorec3.txt")
+vzorec <- read.table("vzorec3.txt")
 
-#narišemo histogram
+#narišemo histogram škodnih zahtevkov
 histogram1 <-hist(vzorec[[1]],breaks = 50,
                   main = "Histogram škodnih zahtevkov",
                   xlab = "Škodni zahtevki",
                   ylab ="Višina škodnih zahtevkov",
                   col ="cornflowerblue")
 
-#b.)
+#b.)#izračun neznanih parametrov
 parametri <- mde(vzorec[[1]], ppareto1, start  = list(shape=1,min =1), measure = "CvM")
-alfa <- parametri$estimate[1]
-min <- parametri$estimate[2]
+alfa <- parametri$estimate[1] # = 1.926127
+min <- parametri$estimate[2] # = 4.065348 
 
 #c.)
-#krivuljo gostote dodamo histogramu
+#krivuljo gostote slučajne spremenljivke Y dodamo histogramu
 histogram1 <-hist(vzorec[[1]],breaks = 50,
                   main = "Histogram škodnih zahtevkov",
                   xlab = "Škodni zahtevki",
@@ -45,21 +46,21 @@ curve(ppareto1(x, shape = alfa,min = min ),
       lwd = 2)
 legend("bottomright",c("Vzorčna funkcija","Porazdeltivena funkcija"),col = c("black","red"),lty = 1)
 
-#d.)
-upanje.Y <- as.numeric( alfa*min/(alfa-1))
+#d.)Izračun upanja slučajne spremenljivke Y
+upanje.Y <- as.numeric( alfa*min/(alfa-1)) # = 8.454971
 upanje.N <- 15
-upanje.S <- upanje.N * upanje.Y
+upanje.S <- upanje.N * upanje.Y #126.8246
 
 var.S <- Inf #ker je var.Y = Inf
 
-############################################################################################
+#==================================================================================
 #2.naloga
 
-#a.)
+#a.)Diskretizacija porazdelitve Y
 
-m <- 1
-M <-40
-h <- 0.5
+m <- 1 #začetek intervala
+M <-40 #konec intervala
+h <- 0.5 #dolžina koraka
 diskretnaY <- discretize(ppareto1(x,shape = alfa, min = min), 
                           from = m, 
                           to = (M + 0.5),
@@ -67,7 +68,7 @@ diskretnaY <- discretize(ppareto1(x,shape = alfa, min = min),
                           method = "rounding"
                           )
 
-diskretnaY2 <- discretize(ppareto1(x,shape = alfa, min = min), 
+diskretnaY2 <- discretize(ppareto1(x,shape = alfa, min = min), #za kasnejšo uporabo pri panjerjevemu algoritmu
                          from = 0, 
                          to = 100000,
                          step = h)
@@ -108,15 +109,16 @@ plot(F.S)
 #d.)S je diskretna spremenljivka
 
 #izračun upanja in disperzije
-E.S <- sum( knots(F.S) * diff(F.S)) #upanje slučanje spremenljivke S = (vrednosti S)* (skoki v teh točkah)
+E.S <- sum( knots(F.S) * diff(F.S)) #upanje slučanje spremenljivke S = (vrednosti S)* (skoki v teh točkah) # = 121.1313
 
-E.S2 <- sum( knots(F.S)^2 * diff(F.S)) # drugi moment slučajne spremenljivke S
-D.S <- E.S2 - E.S^2 #disperzija od S = E(S^2)-E(S)^2
+E.S2 <- sum( knots(F.S)^2 * diff(F.S)) # drugi moment slučajne spremenljivke S # = 17260.9
+D.S <- E.S2 - E.S^2 #disperzija od S = E(S^2)-E(S)^2 # = 2588.114
 
-TV <- VaR(F.S,0.995) #tvegana vrednost
-PI <- CTE(F.S, 0.005) #pričakovani izpad
+TV <- VaR(F.S,0.995) #tvegana vrednost = 392.5
+PI <- CTE(F.S, 0.005) #pričakovani izpad = 121.8433 
 
-##3.naloga
+#===============================================================
+#3.naloga
 
 #a.) simulacija 10000 vrednosti slučajne spremenljivke S
 
@@ -131,12 +133,14 @@ for (n in simN){
 }
 
 #b.)ocena za upanje in disperzijo spremenljivke S
-E.simS <- mean(simS)
-Var.simS <- var(simS)
+E.simS <- mean(simS) #=126.2367
+Var.simS <- var(simS) #= 3926.283
 
+#s simulacijo dobimo boljše ocene za pričakovano vrednost in varianco, kot pa
+#s Panjerjevim algoritmom, saj so vrednosti bližje tistim iz naloge 1.
 
 #c.)Ocena tvegane vrednosti 
-tv_2 <- sort(simS)[9950]
+TV_2 <- sort(simS)[9950] #=398.2419, oceni za tvegano vrednost sta si precej blizu
 
 #d.) 
 plot(ecdf(simS),
